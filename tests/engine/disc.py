72e11dcce148a50eea84645293b77aeba2ed69a7
Beat Reichenbach <44111292+beatreichenbach@users.noreply.github.com>
2026-09-21 16:06:23 -0700

    test(engine): update disc test

--- tests/engine/disc.py
+++ tests/engine/disc.py
@@ -1,33 +1,37 @@
-import numpy as np
 from OpenGL import GL
 from qtpy import QtCore, QtGui
 
+import tests
 from flare.engine.base import Array
-from flare.engine.opengl import OpenGLTask
+from flare.engine.opengl import OpenGLTask, create_context_surface
 from flare.engine.tasks.common import DiscMesh
 from flare.ui import application
-from flare.ui.widgets.viewer import Viewer
+from flare.ui.widgets import Viewer
+
+RESOLUTION = QtCore.QSize(512, 512)
 
 VERTEX_SHADER = """
 #version 430 core
 
-layout (location = 0) in vec2 position;
+layout (location = 0) in vec3 position;
 void main() {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = vec4(position.xy, 0.0, 1.0);
 }
 """
 
 FRAGMENT_SHADER = """
 #version 430 core
 
-out vec4 FragColor;
+out vec4 rgba;
 void main() {
-    FragColor = vec4(1.0);
+    rgba = vec4(1.0);
 }
 """
 
 
-class GridTask(OpenGLTask):
+class DiscTask(OpenGLTask):
+    """Draw a DiscMesh as a wireframe."""
+
     def __init__(self, context: QtGui.QOpenGLContext) -> None:
         super().__init__(context)
 
@@ -35,60 +39,39 @@ class GridTask(OpenGLTask):
         fragment_shader = self.load_shader(FRAGMENT_SHADER, GL.GL_FRAGMENT_SHADER)
 
         self._program = self.create_program(shaders=(vertex_shader, fragment_shader))
-        self._fbo_texture = self.create_texture(clamp_to_border=True)
-        self._fbo = self.create_fbo(self._fbo_texture)
+        self._texture = self.create_texture(
+            clamp_to_border=True, resolution=RESOLUTION.toTuple()
+        )
+        self._fbo = self.create_fbo(self._texture)
         self._vbo = self.create_buffer()
         self._vao = self.create_vao()
 
-    def run(self) -> Array:
-        width, height = 512, 512
+    def run(self, radius: float = 0.8, resolution: int = 8) -> Array:
+        """Return the rendered DiscMesh wireframe."""
 
-        vertices = DiscMesh.get_triangles(radius=0.8, resolution=8)
-        direction = (1, 0)
-        direction /= np.linalg.norm(direction)
-        rotation = np.array(
-            ((direction[0], -direction[1]), (direction[1], direction[0]))
-        )
-        vertices[:, :2] = vertices[:, :2] @ rotation
+        vertices = DiscMesh.get_triangles(radius=radius, resolution=resolution)
         self.update_vbo(self._vbo, vertices)
+        self.setup_vao(self._vao, self._vbo, location=0, components=3)
+        self.bind_vao(self._vao)
 
-        resolution = QtCore.QSize(width, height)
-        self._fbo_texture = self.create_texture(
-            clamp_to_border=True, resolution=resolution.toTuple()
-        )
-        self.update_fbo(self._fbo, self._fbo_texture)
-
-        # Render
         GL.glBindFramebuffer(GL.GL_DRAW_FRAMEBUFFER, self._fbo)
         GL.glClearColor(0.0, 0.0, 0.0, 1.0)
         GL.glClear(GL.GL_COLOR_BUFFER_BIT)
-        GL.glViewport(0, 0, resolution.width(), resolution.height())
-
+        GL.glViewport(0, 0, RESOLUTION.width(), RESOLUTION.height())
         GL.glUseProgram(self._program)
-
         GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE)
-        GL.glDrawArrays(GL.GL_TRIANGLES, 0, vertices.size)
+        GL.glDrawArrays(GL.GL_TRIANGLES, 0, len(vertices))
         GL.glFinish()
 
-        # Copy buffer
-        data = GL.glReadPixels(0, 0, width, height, GL.GL_RGBA, GL.GL_FLOAT)
-        array = np.frombuffer(data, dtype=np.float32).reshape((height, width, 4))
-        array = np.flipud(array)
-
-        image = Array(array=array, args=(self._fbo,))
-        return image
+        array = self.read_texture(self._texture, RESOLUTION)
+        return Array(array=array, args=(self._fbo,))
 
 
 def main() -> None:
     with application():
-        context = QtGui.QOpenGLContext()
-        context.create()
-        surface = QtGui.QOffscreenSurface()
-        surface.create()
-        context.makeCurrent(surface)
+        context, _ = create_context_surface()
 
-        task = GridTask(context)
-        image = task.run()
+        image = DiscTask(context).run()
 
         viewer = Viewer()
         viewer.show()
@@ -96,4 +79,5 @@ def main() -> None:
 
 
 if __name__ == '__main__':
+    tests.init()
     main()
